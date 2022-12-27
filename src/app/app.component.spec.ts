6a938a72bcbff153d6b7aa6931d121321b44907e
Juan Jose Sapuyes <jsapuyesp@gmail.com>
2022-12-27 18:00:45 -0500

    Test succesfully

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
   it(`should have as title 'countriesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('countriesAppp');
+    expect(app.title).toEqual('countriesApp');
   });
 
   it('should have a sidebar', () => {
